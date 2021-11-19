e8635bbd733aa46b19f910de7edd162d1fa258ab
NicDuncanUNF <94423124+NicDuncanUNF@users.noreply.github.com>
2021-11-18 21:05:19 -0500

    Modularity upgrade + getInstrMovement
    Headers.h: Organized where each function is referenced from. Removed class code notes.
    
    instructions.c: Pulled getOpcode from main.c for modularity. Added getInstrMovement which returns which format and by extension the amount the PC moves for each instruction.
    
    symbols.c: Pulled addSymbol from main.c to here for modularity.
    
    Misc: Added various comments among the above to assist in readability.

--- headers.h
+++ headers.h
@@ -25,38 +25,18 @@ typedef struct opcodes OPCODES;
 
 OPCODES OpcodeTable[ 32 ];
 
-int IsAValidSymbol( char *TestSymbol );
-int IsADirective( char *Test );
-int IsAnInstruction( char *Test );
+
+
 int updateLocation(char *dirInst, char* tokThird, int currCount, char fullLine[], int lineNumber);
 void addSymbol(struct symbol* Tab[], int addr, int src, char* symName);
 int symbolExists(struct symbol* Tab[], char* sName);
-char getOpcode(char* nem);
 
-//class code/notes
-/*
-int index;
-index = 0;
-struct symbol newSym;
-newSym = malloc()sizeof(struct symbol);
-newSym.address = addr, newSym.sourceLine =src, strcpy(newSym.name, sName);
-while(Tab[index] != NULL){
-	index++;
-}
-Tab[index] = newSym;
-}
+//function from directives.c
+int IsADirective( char *Test );
 
-int //returns zero or non zero// SymbolExists(struct Symbol* tab[], char* sName){
-int Result = 0;
-int  index = 0;
-while(Tab[index] != NULL){
-	if(strcmp(sName, Tab[index].name) == 0){
-		Result = -1;
-		break;
-	};
-	index++;
-}
+//Functions from Symbols.c
+int IsAValidSymbol( char *TestSymbol );
 
-return result;
-}
-*/
+//Functions from instructions.c
+char getOpcode(char* Instr);
+int IsAnInstruction( char *Test ); //TODO find function and place into file
